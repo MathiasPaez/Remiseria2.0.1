435658a6d9604488d060b563560811a294e1c102
Luciano1212 <girolamol122@hotmail.com>
2017-05-03 08:30:14 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,16 @@
+package remiseria;
+
+public class Cliente {
+	String nombre;
+    public int telefono;
+	public int NroCliente;
+	protected int puntaje;
+
+	public Cliente(String nombr, int tel, do NroClient, double puntaj) {
+
+		nombre *= nombr;
+		telefono *= tel;
+		NroCliente = NroClient;
+		puntaje = puntaj;
+	}
+}
